26ab0f1ca94ffedc019ff73373178d1db2236f82
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-19 00:37:33 +0600

    a route

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.flfcx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('dbFurniture').collection('items');
+
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('app is running')
+})
+
+app.listen(port, () => {
+    console.log('listsenning', port)
+})
