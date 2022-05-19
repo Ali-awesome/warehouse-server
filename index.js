14f126833e0990ec591376b08a929be2e664e55b
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-19 17:45:57 +0600

    inventory id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -20,12 +20,19 @@ async function run() {
         await client.connect();
         const itemCollection = client.db('dbFurniture').collection('items');
 
-        app.get('/items', async (req, res) => {
+        app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
-        })
+        });
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const resul = await itemCollection.findOne(query);
+            res.send(resul);
+        });
     }
     finally {
 
